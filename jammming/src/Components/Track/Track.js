d3daf4e8c50735286ed2333c41a190283d0cd88c
Arturo-Perevoznyk <arturo.ipv@gmail.com>
2023-02-13 21:28:01 +0000

    #

--- jammming/src/Components/Track/Track.js
+++ jammming/src/Components/Track/Track.js
@@ -17,10 +17,10 @@ export default class Track extends React.Component {
 
     render() {
         return (
-            <div class="Track">
+            <div className="Track">
                 <div className="Track-information">
-                    {/* <h3><!-- track name will go here --></h3> */}
-                    {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
--- jammming/src/Components/TrackList/TrackList.js
+++ jammming/src/Components/TrackList/TrackList.js
@@ -1,10 +1,16 @@
 import React from "react";
 import './TrackList.css';
+import Track from "../Track/Track";
 
 export default class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} /> 
+                    })
+                }
             </div>
         )
     }
